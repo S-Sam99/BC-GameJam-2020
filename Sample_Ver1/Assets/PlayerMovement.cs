e344cdfe821c7960c1b9c863295d913a437ff398
StevenASam <56138951+StevenASam@users.noreply.github.com>
2020-02-15 01:45:38 -0800

    Updated camera and movement

---
+++ Sample_Ver1/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody rb;
+
+    public float ForwardMovement = 2000f;
+    public float SideMovement = 200f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, ForwardMovement * Time.deltaTime);
+
+        if (Input.GetKey("d")){
+           rb.AddForce(SideMovement, 0, 0);
+        }
+
+        if (Input.GetKey("a")){
+            rb.AddForce(-SideMovement, 0, 0);
+        }
+    }
+}
