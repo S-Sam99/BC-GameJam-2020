0fc6595185113c81d992b04e30a3c1b3a47c4100
mattfung99 <official.matthiasfung@gmail.com>
2020-02-15 01:52:52 -0800

    Matt's player movement and camera movement

--- Sample_Ver1/Assets/PlayerMovement.cs
+++ Sample_Ver1/Assets/PlayerMovement.cs
@@ -1,25 +1,51 @@
-﻿using UnityEngine;
+﻿/* Using Libraries */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+/**
+ *   Filename: PlayerMovement.cs
+ *   Contributor: Matt Fung
+ *   Date: February 14, 2020
+*/
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /* Define Instance Fields */
+    public Rigidbody PlayerRigidBody;
+    public float forwardForce = 200f;
+    public float sidewaysForce = 500f;
 
-    public Rigidbody rb;
+    /**
+        Start is called before the first frame update
+        void Start()
+        {
+            // Debug.Log("Testing");
+            // PlayerRigidBody.useGravity = false;
+            PlayerRigidBody.AddForce(0, 200, 500);          // 0 force in x-dir, 200 force in y-dir, 500 in z-dir
+        }
+    */
 
-    public float ForwardMovement = 2000f;
-    public float SideMovement = 200f;
-
-    // Update is called once per frame
-    void Update()
+    /**
+     * Function: FixedUpdate()
+     * Purpose: Player Movement is updated during each fixed frame
+     *          This allows smoother animation between frames
+     *          May have noticeable differences on different machines
+     */
+    void FixedUpdate()
     {
-        rb.AddForce(0, 0, ForwardMovement * Time.deltaTime);
+        // Adding forward force
+        PlayerRigidBody.AddForce(0, 0, forwardForce * Time.deltaTime);       // Adjusts framerate of player movement
 
-        if (Input.GetKey("d")){
-           rb.AddForce(SideMovement, 0, 0);
+        // Keypress to move player to the right
+        if (Input.GetKey("d"))
+        {
+            PlayerRigidBody.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey("a")){
-            rb.AddForce(-SideMovement, 0, 0);
+        // Keypress to move player to the left
+        if (Input.GetKey("a"))
+        {
+            PlayerRigidBody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
         }
     }
 }
