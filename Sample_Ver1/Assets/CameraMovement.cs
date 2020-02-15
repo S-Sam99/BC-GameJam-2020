9e77b37e78a3dcca1c57d8a2512d9eae472cda23
mattfung99 <official.matthiasfung@gmail.com>
2020-02-15 05:49:58 -0800

    Addition of moving obstacles and obstacle collision

--- Sample_Ver1/Assets/CameraMovement.cs
+++ Sample_Ver1/Assets/CameraMovement.cs
@@ -14,13 +14,15 @@ public class CameraMovement : MonoBehaviour
     public Transform player;
     public Vector3 offset;
     public float rotationSpeed = 5.0f;
-
-    public float MaxTilt = 30;
-
     float direction;
 
-    // Update is called once per frame
-    void Update()
+    /**
+     * Function: FixedUpdate()
+     * Purpose: Camera Movement is updated during each fixed frame
+     *          Camera follows the player to mimic a first person view
+     *          May have noticeable differences on different machines
+     */
+    void FixedUpdate()
     {
         // Place camera slightly behind the player and follow the player
         transform.position = player.position + offset;
@@ -31,8 +33,10 @@ public class CameraMovement : MonoBehaviour
         // Rotates clockwise
         if ((Input.GetKeyDown("l")))
             direction += 10;
+
         // if (direction >= 50 || direction <= -50)
         //     direction = 0;
+
         // Updates rotation
         transform.Rotate(0, 0, direction * Time.deltaTime, Space.Self);
     }
