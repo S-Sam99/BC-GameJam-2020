30ba2f486a07753a46345f2c3be5b6d5add10559
mattfung99 <official.matthiasfung@gmail.com>
2020-02-15 02:18:15 -0800

    Compatibility fix

---
+++ Sample_Ver1/Assets/CameraMovement.cs
@@ -0,0 +1,22 @@
+﻿/* Using Libraries */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *   Filename: CameraMovement.cs
+ *   Contributor: Matt Fung
+ *   Date: February 15, 2020
+*/
+public class CameraMovement : MonoBehaviour
+{
+    /* Define Instance Fields */
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
