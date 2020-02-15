a3e8fccab1c2f44bb630db1d5016cd4d31111ab0
StevenASam <56138951+StevenASam@users.noreply.github.com>
2020-02-15 05:42:58 -0800

    Added onto Daniel's work

---
+++ Game/Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody rb;
+    public float ForwardMovement = 2000f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, ForwardMovement * Time.deltaTime);
+        
+    }
+}
+
---
+++ Game/Assets/Tubular.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tubular : MonoBehaviour
+{
+    public float MoveZ = 1f;
+    public GameObject go;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z <= 250) { 
+        go.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + MoveZ);
+    }
+    }
+}
