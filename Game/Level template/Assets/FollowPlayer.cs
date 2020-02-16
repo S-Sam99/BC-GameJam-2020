1e8ad3fa56055f9ca74c8d3c5233467ca2ca1f1c
StevenASam <56138951+StevenASam@users.noreply.github.com>
2020-02-16 10:08:18 -0800

    Adding confused level

---
+++ Game/Level template/Assets/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float spin = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+       // transform.Rotate(0, 0, -spin * Time.deltaTime);
+
+    }
+
+}
---
+++ Game/Level template/Assets/Tubular.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tubular : MonoBehaviour
+{
+  public float spin = 5f;
+  // Update is called once per frame
+  void Update()
+  {
+
+      if (Input.GetKey(KeyCode.RightArrow))
+      {
+         transform.Rotate(new Vector3(0,0, -spin));
+      }
+
+      if (Input.GetKey(KeyCode.LeftArrow))
+      {
+         transform.Rotate(new Vector3(0,0, spin));
+      }
+
+  }
+}
